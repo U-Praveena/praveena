2e0c058a31cd191461fca86ea6ccdaefc871af70
U-Praveena <120449766+U-Praveena@users.noreply.github.com>
2022-12-14 21:30:39 +0530

    Add files via upload

---
+++ puzzle5.py
@@ -0,0 +1,7 @@
+def test(str1):
+    return str1[len(str1)-2] in str[len(str1)-1] and str1[len(str1)-2]!= str[len(str)-1]
+str1=["a","abb","sfs","oo","De","sfde"]
+print("list:")
+print(str1)
+print("check n^th-1 string is a proper substring of n^th string:")
+print(test(str1))
